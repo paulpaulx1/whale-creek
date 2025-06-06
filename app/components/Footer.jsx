81bba756a13ca2554fbacce386f5deb755579039
Paul Neenan <paulneenan@Pauls-MBP.lan>
2025-06-06 16:45:39 -0400

    styles footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -9,38 +9,59 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.container}>
         
-        {/* Hero logo section - much bigger and centered */}
-        <div className={styles.heroSection}>
-          <div className={styles.whaleLogoHero}>
-            <Image 
-              className={styles.logoImage} 
-              width={120} 
-              height={120} 
-              src="/images/whaleCreek.png" 
-              alt="Whale Creek Logo" 
-            />
+        {/* Asymmetrical main content */}
+        <div className={styles.mainContent}>
+          
+          {/* Left side - Logo section */}
+          <div className={styles.logoSection}>
+            <div className={styles.whaleLogoHero}>
+              <Image 
+                className={styles.logoImage} 
+                width={120} 
+                height={120} 
+                src="/images/whaleCreek.png" 
+                alt="Whale Creek Logo" 
+              />
+            </div>
+            <h2 className={styles.heroTitle}>Whale Creek Co</h2>
+            <p className={styles.heroSubtitle}>Indianapolis&apos; Premier Millwork & Construction</p>
           </div>
-          <h2 className={styles.heroTitle}>Whale Creek Co</h2>
-          <p className={styles.heroSubtitle}>Indianapolis&apos; Premier Millwork & Construction</p>
+
+          {/* Right side content */}
+          <div className={styles.rightContent}>
+            
+            {/* Contact grid */}
+            <div className={styles.contactGrid}>
+              <div className={styles.contactItem}>
+                <h4>Call</h4>
+                <a href="tel:3174312449">(317) 431-2449</a>
+              </div>
+              <div className={styles.contactItem}>
+                <h4>Email</h4>
+                <a href="mailto:dave@whalecreek.co">dave@whalecreek.co</a>
+              </div>
+              <div className={styles.contactItem}>
+                <h4>Service</h4>
+                <span>Indianapolis & Surrounding Areas</span>
+              </div>
+            </div>
+
+            {/* Quick links */}
+            <div className={styles.quickLinks}>
+              <h4>Quick Links</h4>
+              <div className={styles.linksList}>
+                <Link href="/about">About</Link>
+                <Link href="/services">Services</Link>
+                <Link href="/project-gallery">Gallery</Link>
+                <Link href="/contact">Contact</Link>
+                <Link href="/blog">Blog</Link>
+              </div>
+            </div>
+
+          </div>
         </div>
 
-        {/* Simple contact grid */}
-        <div className={styles.contactGrid}>
-          <div className={styles.contactItem}>
-            <h4>Call</h4>
-            <a href="tel:3174312449">(317) 431-2449</a>
-          </div>
-          <div className={styles.contactItem}>
-            <h4>Email</h4>
-            <a href="mailto:info@whalecreekwoodworking.com">dave@whalecreek.co</a>
-          </div>
-          <div className={styles.contactItem}>
-            <h4>Service</h4>
-            <span>Indianapolis & Surrounding Areas</span>
-          </div>
-        </div>
-
-        {/* Simple bottom bar */}
+        {/* Bottom bar */}
         <div className={styles.bottomBar}>
           <p className={styles.copyright}>© 2025 Whale Creek Co</p>
           <p className={styles.designedBy}>
