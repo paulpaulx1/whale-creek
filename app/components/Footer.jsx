055b12a9ef79a2e5fee46974e6e010ec1bb13934
maxx-css <hellomauriciorivera@gmail.com>
2025-06-26 13:53:14 -0400

    Social icons added to footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,42 +3,45 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './Footer.module.css';
+import SocialIcons from './SocialIcons';
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        
         {/* Asymmetrical main content */}
         <div className={styles.mainContent}>
-          
           {/* Left side - Logo section */}
           <div className={styles.logoSection}>
             <div className={styles.whaleLogoHero}>
-              <Image 
-                className={styles.logoImage} 
-                width={120} 
-                height={120} 
-                src="/images/whaleCreek.png" 
-                alt="Whale Creek Logo" 
+              <Image
+                className={styles.logoImage}
+                width={120}
+                height={120}
+                src='/images/whaleCreek.png'
+                alt='Whale Creek Logo'
               />
             </div>
             <h2 className={styles.heroTitle}>Whale Creek Co</h2>
-            <p className={styles.heroSubtitle}>Indianapolis&apos; Premier Millwork & Construction</p>
+            <p className={styles.heroSubtitle}>
+              Indianapolis&apos; Premier Millwork & Construction
+            </p>
+            <div className={styles.socialSection}>
+              <SocialIcons />
+            </div>
           </div>
 
           {/* Right side content */}
           <div className={styles.rightContent}>
-            
             {/* Contact grid */}
             <div className={styles.contactGrid}>
               <div className={styles.contactItem}>
                 <h4>Call</h4>
-                <a href="tel:3174312449">(317) 431-2449</a>
+                <a href='tel:3174312449'>(317) 431-2449</a>
               </div>
               <div className={styles.contactItem}>
                 <h4>Email</h4>
-                <a href="mailto:dave@whalecreek.co">dave@whalecreek.co</a>
+                <a href='mailto:dave@whalecreek.co'>dave@whalecreek.co</a>
               </div>
               <div className={styles.contactItem}>
                 <h4>Service</h4>
@@ -50,14 +53,13 @@ const Footer = () => {
             <div className={styles.quickLinks}>
               <h4>Quick Links</h4>
               <div className={styles.linksList}>
-                <Link href="/about">About</Link>
-                <Link href="/indianapolis-general-contractor">Services</Link>
-                <Link href="/project-gallery">Gallery</Link>
-                <Link href="/contact">Contact</Link>
-                <Link href="/blog">Blog</Link>
+                <Link href='/about'>About</Link>
+                <Link href='/indianapolis-general-contractor'>Services</Link>
+                <Link href='/project-gallery'>Gallery</Link>
+                <Link href='/contact'>Contact</Link>
+                <Link href='/blog'>Blog</Link>
               </div>
             </div>
-
           </div>
         </div>
 
@@ -65,7 +67,14 @@ const Footer = () => {
         <div className={styles.bottomBar}>
           <p className={styles.copyright}>© 2025 Whale Creek Co</p>
           <p className={styles.designedBy}>
-            Designed by <a href="https://paxmedia.com" target="_blank" rel="noopener noreferrer">Pax Media</a>
+            Designed by{' '}
+            <a
+              href='https://paxmedia.com'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Pax Media
+            </a>
           </p>
         </div>
       </div>
