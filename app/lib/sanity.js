9273ad622a3de2875a14bab5799c2c2c7aad42e4
paulpaulx1 <paulmneenan@gmail.com>
2026-04-28 09:01:28 -0400

    usecdn false

--- app/lib/sanity.js
+++ app/lib/sanity.js
@@ -4,7 +4,7 @@ import imageUrlBuilder from '@sanity/image-url';
 export const client = createClient({
   projectId: '95dw463o',
   dataset: 'production',
-  useCdn: true,
+  useCdn: false,
   apiVersion: '2023-05-03',
 });
 
